f1d596844180b08e1898d67f84e95173118944b6
Khamdamov <58900685+diyorbek57@users.noreply.github.com>
2022-07-07 11:16:02 +0500

    //

--- app/src/main/java/com/ayizor/afeme/api/main/Client.kt
+++ app/src/main/java/com/ayizor/afeme/api/main/Client.kt
@@ -7,6 +7,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class Client {
@@ -16,9 +17,14 @@ class Client {
         fun getClient(context: Context): Retrofit? {
             if (retrofit == null) {
                 var log = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-                val client = OkHttpClient.Builder().addInterceptor(
-                    HeaderInterceptor(PrefsManager(context).loadUserRegisteredToken().toString())
-                ).addInterceptor(log).build()
+                val client = OkHttpClient.Builder()
+                    .connectTimeout(40, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60, TimeUnit.SECONDS).addInterceptor(
+                        HeaderInterceptor(
+                            PrefsManager(context).loadUserRegisteredToken().toString()
+                        )
+                    ).addInterceptor(log).build()
                 retrofit = Retrofit.Builder()
                     .baseUrl(Api.BASE_URL).client(client)
                     .addConverterFactory(ScalarsConverterFactory.create())
